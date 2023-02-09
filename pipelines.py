365debb4b44913b17035a6e42f720387399826b6
Jonathan Saragosti <saragostij+github@gmail.com>
2023-02-09 00:48:42 -0500

    Create pipelines.py

---
+++ pipelines.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.pipeline import Pipeline
+
+embedding_pipe = Pipeline(
+    steps=[
+        ("ordinal_encoder", OrdinalEncoder(
+            categories="auto",
+            dtype=int,
+            handle_unknown="use_encoded_value",
+            unknown_value=-1,
+            encoded_missing_value=-2,            
+            )
+        )
+    ]
+).set_output(transform="pandas")
