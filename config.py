24cf9d5b7e9f2ae7ec317d621e094ead1c9a2224
Jonathan Saragosti <saragostij+github@gmail.com>
2023-02-09 19:46:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
+from dataclasses import dataclass
 from logging import getLogger, INFO, StreamHandler, FileHandler, Formatter
-
 
 
 DATA_DIR_PATH = "data"
@@ -11,14 +11,21 @@ US_EURO_SIZE_THRESHOLD = 32
 
 
 def get_logger(use_file:bool = False, name=None):  
-    logger = getLogger(__name__)
-    logger.setLevel(INFO)
-    handler1 = StreamHandler()
-    handler1.setFormatter(Formatter('[%(asctime)s](%(name)s) | %(levelname)s: %(message)s'))
-    logger.addHandler(handler1)
-    if use_file:
-        filename = OUTPUT_DIR +'train'
-        handler2 = FileHandler(filename=f"{filename}.log")
-        handler2.setFormatter(Formatter("%(message)s"))
-        logger.addHandler(handler2)
-    return logger
+	logger = getLogger(__name__)
+	logger.setLevel(INFO)
+	handler1 = StreamHandler()
+	handler1.setFormatter(Formatter('[%(asctime)s](%(name)s) | %(levelname)s: %(message)s'))
+	logger.addHandler(handler1)
+	return logger
+
+@dataclass
+class ModelConfig:
+	test_size: float = 0.3
+	embedding_dim: int = 5
+	learning_rate: float = 0.0005
+	batch_size: int = 512
+	checkpoint_path: str = "/tmp/model_checkpoint"
+	validation_split: float = 0.3
+	epochs: int = 20
+	fit_verbose: int = 1
+
