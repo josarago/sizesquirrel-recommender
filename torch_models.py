0e2fbc6ab893bc75a2d26eb1ff0d301b2acff103
Jonathan Saragosti <saragostij+github@gmail.com>
2023-02-24 12:16:37 -0500

    Update torch_models.py

--- torch_models.py
+++ torch_models.py
@@ -1,14 +1,26 @@
 import torch
+from torch import nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from config import RegressorConfig, ClassifierConfig
 from trainer import Trainer
-from pipelines import EMBEDDING_COLUMNS
+from pipelines import TARGET_CATEGORIES, EMBEDDING_COLUMNS
 
 
-class RatingPredictor(torch.nn.Module):
+class RatingScaler(nn.Module):
+    def __init__(self):
+        super().__init__()  # init the base class
+
+    def forward(self, input):
+        """
+        Forward pass of the function.
+        """
+        return (len(TARGET_CATEGORIES) - 1) * nn.Sigmoid()(input) + 1
+
+
+class RatingPredictor(nn.Module):
     def __init__(
         self,
         embedding_dim,
@@ -29,19 +41,20 @@ class RatingPredictor(torch.nn.Module):
         # embedding inputs
         for name, vocabulary in vocabularies.items():
             vocab_size = len(vocabulary)
-            self.embeddings[name] = torch.nn.Embedding(
+            self.embeddings[name] = nn.Embedding(
                 vocab_size, embedding_dim, sparse=False
             )
-            self.biases[name] = torch.nn.Embedding(vocab_size, 1, sparse=False)
-        self.user_features_stack = torch.nn.Sequential(
-            torch.nn.Linear(self.user_features_dim, self.embedding_dim, dtype=float),
-            torch.nn.ReLU(),
+            self.biases[name] = nn.Embedding(vocab_size, 1, sparse=False)
+        self.user_features_stack = nn.Sequential(
+            nn.Linear(self.user_features_dim, self.embedding_dim, dtype=float),
+            nn.ReLU(),
         )
 
-        self.sku_features_stack = torch.nn.Sequential(
-            torch.nn.Linear(self.sku_features_dim, self.embedding_dim, dtype=float),
-            torch.nn.ReLU(),
+        self.sku_features_stack = nn.Sequential(
+            nn.Linear(self.sku_features_dim, self.embedding_dim, dtype=float),
+            nn.ReLU(),
         )
+        self.rating_scaler = RatingScaler()
 
     def forward(self, inputs: dict) -> torch.tensor:
         # embedding and biases
@@ -49,22 +62,18 @@ class RatingPredictor(torch.nn.Module):
         x_bias_inputs = dict()
         for name in self.vocabularies.keys():
             x_embed_inputs[name] = torch.squeeze(
-                self.embeddings[name](torch.Tensor(inputs[name].values).long()), dim=1
+                self.embeddings[name](inputs[name]), dim=1
             )
-            x_bias_inputs[name] = torch.squeeze(
-                self.biases[name](torch.Tensor(inputs[name].values).long()), dim=1
-            )
+            x_bias_inputs[name] = torch.squeeze(self.biases[name](inputs[name]), dim=1)
         # user
         # features
-        x_features_user = self.user_features_stack(
-            torch.tensor(inputs["user_features"].values)
-        )
+        x_features_user = self.user_features_stack(inputs["user_features"])
         # sum all embeddings
         x_embed_user = torch.sum(
             torch.stack(
                 [
                     x_embed_inputs[name]
-                    for name, x in inputs.items()
+                    for name in inputs.keys()
                     if name in EMBEDDING_COLUMNS["user"]
                 ]
                 + [x_features_user]
@@ -77,7 +86,7 @@ class RatingPredictor(torch.nn.Module):
                 torch.stack(
                     [
                         x_bias_inputs[name]
-                        for name, x in inputs.items()
+                        for name in inputs.keys()
                         if name in EMBEDDING_COLUMNS["user"]
                     ]
                 ),
@@ -88,15 +97,13 @@ class RatingPredictor(torch.nn.Module):
 
         # sku
         # features
-        x_features_sku = self.sku_features_stack(
-            torch.tensor(inputs["sku_features"].values)
-        )
+        x_features_sku = self.sku_features_stack(inputs["sku_features"])
         # sum all sku embeddings
         x_embed_sku = torch.sum(
             torch.stack(
                 [
                     x_embed_inputs[name]
-                    for name, x in inputs.items()
+                    for name in inputs.keys()
                     if name in EMBEDDING_COLUMNS["sku"]
                 ]
                 + [x_features_sku]
@@ -109,7 +116,7 @@ class RatingPredictor(torch.nn.Module):
                 torch.stack(
                     [
                         x_bias_inputs[name]
-                        for name, x in inputs.items()
+                        for name in inputs.keys()
                         if name in EMBEDDING_COLUMNS["sku"]
                     ]
                 ),
@@ -127,7 +134,39 @@ class RatingPredictor(torch.nn.Module):
         else:
             raise ValueError("dot or subtract")
         with_biases = torch.sum(torch.stack([x_bias_user, x_bias_sku, combined]), dim=0)
-        return with_biases
+        out = self.rating_scaler(with_biases)
+        return out
+
+
+def train():
+    for epoch in range(n_epoch):
+        _loss_values = dict(train=[])
+
+        for X_train, y_train in train_data_loader:
+            # Compute prediction error
+            y_pred_train = self._model(X_train.to(self._training_device))
+            train_loss = self._loss_fn(y_pred_train, y_train.to(self._training_device))
+            _loss_values["train"].append(train_loss.item())
+
+            # Backpropagation
+            self._optimizer.zero_grad()
+            train_loss.backward()
+            self._optimizer.step()
+
+        self._loss_values["train"].append(np.sqrt(np.mean(_loss_values["train"])))
+        if params["with_validation"]:
+            _loss_values["val"] = []
+            self._model.eval()  # Optional when not using Model Specific layer
+            for X_val, y_val in val_data_loader:
+                # Forward Pass
+                y_pred_val = self._model(X_val)
+                # Find the Loss
+                val_loss = self._loss_fn(y_pred_val, y_val)
+                # Calculate Loss
+                _loss_values["val"].append(val_loss.item())
+        self._loss_values["val"].append(np.sqrt(np.mean(_loss_values["val"])))
+
+    print("Training Complete")
 
 
 if __name__ == "__main__":
